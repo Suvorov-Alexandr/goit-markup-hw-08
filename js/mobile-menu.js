6feba17e51ab916a91752292cf4a4e250f6884df
Suvorov-Alexandr <85122241+Suvorov-Alexandr@users.noreply.github.com>
2021-08-11 14:59:56 +0300

    12
    js

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,6 +1,7 @@
 (() => {
     const menuBtnRef = document.querySelector("[data-menu-button]");
     const mobileMenuRef = document.querySelector("[data-menu]");
+    const bodyRef = document.querySelector("[data-body]");
 
     menuBtnRef.addEventListener("click", () => {
         const expanded =
@@ -10,6 +11,7 @@
         menuBtnRef.setAttribute("aria-expanded", !expanded);
 
         mobileMenuRef.classList.toggle("is-open");
+        bodyRef.classList.toggle("is-scrolling-disabled");
 
         });
 })();
